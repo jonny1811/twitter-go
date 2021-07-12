27c9c638b3fd9209d13b7d68aa1aed1f58841459
Jonny D. Lezcano <jonny@quanto.la>
2021-07-11 22:55:24 -0400

    verPerfil

---
+++ jwt/jwt.go
@@ -0,0 +1,31 @@
+package jwt
+
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/jonny1811/twitter-go/models"
+)
+
+func GeneroJWT(t models.Usuario) (string, error) {
+	miClave := []byte("MastersdelDesarrolo_grupoFace")
+
+	payload := jwt.MapClaims{
+		"email":            t.Email,
+		"nombre":           t.Nombre,
+		"apellidos":        t.Apellidos,
+		"fecha_nacimiento": t.FechaNacimiento,
+		"biografia":        t.Biografia,
+		"ubicacion":        t.Ubicacion,
+		"sitioWeb":         t.SitioWeb,
+		"_id":              t.ID.Hex(),
+		"exp":              time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	tokenStr, err := token.SignedString(miClave)
+	if err != nil {
+		return tokenStr, err
+	}
+	return tokenStr, nil
+}
